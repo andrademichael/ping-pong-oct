917f15cc53390e5b131afd63f0dfd0fa5b0d2112
Michael E. Andrade <andrade.michael.e@gmail.com>
2016-10-28 10:32:14 -0700

    input form fixed in js and html to properly detect number input

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 //backend
 var pingPong = function(number) {
-  outputs = [];
-  return outputs;
-  };
+  numbers = [];
+  // for (i=0; i<number.length)
+  return numbers;
 };
 //frontend
 
-$(function() {
-  event.preventDefault();
-  $(".inputForm").submit(function() {
-    var input = parseInt($("#inputNum").val());
-    if (!input) {
+$(document).ready(function() {
+  $("form#inputForm").submit(function() {
+    event.preventDefault();
+    debugger;
+    var userNum = parseInt($("input#numberIn").val());
+    if (!userNum) {
       alert("Please enter a number.")
+    } else {
+      outputs = pingPong(userNum);
+      for (i=0; i < outputs.length; i++) {
+          $("#outputList").append("<li>" + outputs[i] + "</li");
+      };
     };
   });
 });
